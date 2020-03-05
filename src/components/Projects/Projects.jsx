271bab0a9bfc716307f91cd5519ca8ebc2c42948
StrkerXx798 <str1k4@yandex.ru>
2020-03-05 19:06:35 +0300

    Projects page and Project component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Projects.module.css';
+import Project from './Project/Project';
 
 
 const Projects = (props) => {
     return (
         <div className = {styles.projects}>
-
+            <div className = {styles.container}>
+                <div className={styles.heading}>
+                    <span>My Projects</span>
+                </div>
+                <div className = {styles.projectsSet}>
+                    <Project />
+                    <Project />
+                </div>
+            </div>
         </div>
     );
 }
