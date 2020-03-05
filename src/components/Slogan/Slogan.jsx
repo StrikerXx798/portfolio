2522aebc87730574864a8b6cfb936f6df502e14b
StrkerXx798 <str1k4@yandex.ru>
2020-03-05 19:07:14 +0300

    Projects page and Project component

--- src/components/Slogan/Slogan.jsx
+++ src/components/Slogan/Slogan.jsx
@@ -5,7 +5,18 @@ import styles from './Slogan.module.css';
 const Slogan = (props) => {
     return (
         <div className = {styles.slogan}>
-
+            <div className = {styles.container}>
+                <div className = {styles.sloganText}>
+                <span>
+                    I consider options for remote work
+                </span>
+                </div>
+                <div className = {styles.button}>
+                    <button>
+                        Recruit Me
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
