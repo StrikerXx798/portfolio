d2d060a589c89e6a0a172dce4fa8ef019a2ecfaf
StrkerXx798 <str1k4@yandex.ru>
2020-03-05 20:25:23 +0300

    Contacts and Footer page and their components

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,8 +4,24 @@ import styles from './Contacts.module.css';
 
 const Contacts = (props) => {
     return (
-        <div className = {styles.contacts}>
-
+        <div className={styles.contacts}>
+            <div className={styles.container}>
+                <div className={styles.contactForm}>
+                    <div className={styles.heading}>
+                        <span>
+                            My contacts
+                        </span>
+                    </div>
+                    <form className={styles.inputForm}>
+                        <input placeholder="Your Name" type="text"/>
+                        <input placeholder="Your E-Mail" type="text"/>
+                        <textarea placeholder="Write your comment..."></textarea>
+                    </form>
+                    <div className={styles.submitButton}>
+                        <button type="submit">Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,25 @@ import styles from './Footer.module.css';
 
 const Footer = (props) => {
     return (
-        <div className = {styles.footer}>
-
+        <div className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.heading}>
+                      <span>
+                          Evgeny Turetskov
+                      </span>
+                </div>
+                <div className={styles.messengers}>
+                      <div className={styles.messenger}></div>
+                      <div className={styles.messenger}></div>
+                      <div className={styles.messenger}></div>
+                      <div className={styles.messenger}></div>
+                </div>
+                <div className={styles.rights}>
+                    <span>
+                        © 2020 All rights reserved
+                    </span>
+                </div>
+            </div>
         </div>
     );
 }
