a91776ea176a6daaa1af8a2cafd7911cfc5f6950
StrikerXx798 <str1k4@yandex.ru>
2020-05-07 22:03:06 +0300

    Updates

---
+++ precache-manifest.1135fde6e5a97299fee1566ecd865f69.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5660a6b6f9dce85e8f9064b624e9f0ed",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "38674723a49ce232e88b",
+    "url": "/portfolio/static/css/main.53324d4a.chunk.css"
+  },
+  {
+    "revision": "6644991f91f68cc0231b",
+    "url": "/portfolio/static/js/2.295ce393.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.295ce393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38674723a49ce232e88b",
+    "url": "/portfolio/static/js/main.14a0fa4c.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "f7c900e03caad55a68d32f9370975ff3",
+    "url": "/portfolio/static/media/fb.f7c900e0.svg"
+  },
+  {
+    "revision": "5d0f37164e160275696313e8f4ffee08",
+    "url": "/portfolio/static/media/telegram.5d0f3716.svg"
+  },
+  {
+    "revision": "bff64b2f26a59ca3dba2256a2536cbb7",
+    "url": "/portfolio/static/media/twitter.bff64b2f.svg"
+  },
+  {
+    "revision": "83d1a25c4e1678dbe97395076ed4bc8d",
+    "url": "/portfolio/static/media/untitled(10).83d1a25c.svg"
+  },
+  {
+    "revision": "49b310169010d882ce93e245c173d7bd",
+    "url": "/portfolio/static/media/untitled(3).49b31016.svg"
+  },
+  {
+    "revision": "a19849ce879c5105b4f19243793d2e0d",
+    "url": "/portfolio/static/media/untitled(5).a19849ce.svg"
+  },
+  {
+    "revision": "9a507a11b135abe51aa920528afdc3ca",
+    "url": "/portfolio/static/media/untitled(7).9a507a11.svg"
+  },
+  {
+    "revision": "2097e7af9e4cedf515615a6dc14bfcce",
+    "url": "/portfolio/static/media/untitled(8).2097e7af.svg"
+  },
+  {
+    "revision": "2be40f992c7a6151dbf0dfa58448b896",
+    "url": "/portfolio/static/media/untitled(9).2be40f99.svg"
+  },
+  {
+    "revision": "48ef585897d7a1ecb18f18bc5e99f038",
+    "url": "/portfolio/static/media/vk.48ef5858.svg"
+  }
+]);
